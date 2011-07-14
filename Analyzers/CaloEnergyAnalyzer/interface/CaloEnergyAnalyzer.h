4ba1d0b080912d2ceca775caeb473ac8b840e922
mojoe137 <>
2011-07-14 11:37:14 +0000

    Calculate et and eta event by event using the primary vertex.

--- Analyzers/CaloEnergyAnalyzer/interface/CaloEnergyAnalyzer.h
+++ Analyzers/CaloEnergyAnalyzer/interface/CaloEnergyAnalyzer.h
@@ -9,16 +9,18 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
-#include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
 #include "DataFormats/HeavyIonEvent/interface/CentralityProvider.h"
 #include "DataFormats/CaloTowers/interface/CaloTowerCollection.h"
 #include "FWCore/Framework/interface/EventSetup.h"
-#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
+//#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
 
 //TFile Service
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "DataFormats/Math/interface/Point3D.h"
 
 //Root Classes
 #include "TTree.h"
@@ -36,36 +38,32 @@ public:
 private:
 	virtual void beginJob();
 	virtual void analyze(const edm::Event&, const edm::EventSetup&);
+	bool getVertexData(const VertexCollection*, float*);
 
 	bool etaBinSize, HFCorrection, realHFbins,noiseCut;
 	int RunData[4];
-	int CaloSize;
-	float *CaloEt;
-	float *CaloEnergy;
-	float *CaloEta;
-	float *CaloPhi;
+//	int CaloSize;
+//	float *CaloEmEnergy;
+//	float *CaloHadEnergy;
+//	float *CaloEta;
+//	float *CaloPhi;
 	float CalodEtdEta[82];
+	float VertData[6];
 	float cent[2];
 	int centi;	
 	
 	float etabin[83];
 	float HFcor[11];
 	
-//	const ZDCDigiCollection *ZDCDigiz;
-//	const ZDCRecHitCollection *ZDCRecHitz;
-//	const HBHERecHitCollection *HBHERecHitz;
-//	const HFRecHitCollection *HFRecHitz;
-//	const CastorRecHitCollection *CastorRecHitz;
 //	const CaloTowerCollection *CaloTowerz;
 	CentralityProvider *CentProv;
 	
 	edm::Service<TFileService> mFileServer;
 	
-//	TTree* ZDCTree;
 	TTree* RunTree;
 	TTree* CenTree;
-//	TTree* ForwardTree;
 	TTree* CaloTree;
+	TTree* VertTree;
 };
 DEFINE_FWK_MODULE(CaloEnergyAnalyzer);
 
