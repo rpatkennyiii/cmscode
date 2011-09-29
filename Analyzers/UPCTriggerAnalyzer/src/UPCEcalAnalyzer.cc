d61584885f4bcad00374694720c3dcf8510328c0
mojoe137 <>
2011-09-29 13:08:09 +0000

    Fix problem with input tag for the EcalRecHit collection

--- Analyzers/UPCTriggerAnalyzer/src/UPCEcalAnalyzer.cc
+++ Analyzers/UPCTriggerAnalyzer/src/UPCEcalAnalyzer.cc
@@ -15,18 +15,21 @@ void UPCEcalAnalyzer::beginJob(){
 	string EcalSecName = EcalColName.substr(11);
    	UPCEcalTree = new TTree(("UPC"+EcalSecName+"Tree").c_str(),("UPC"+EcalSecName+"Tree").c_str());
 	UPCEcalTree->Branch(("n"+EcalSecName).c_str(),&nEcal,("n"+EcalSecName+"/i").c_str());
-	UPCEcalTree->Branch((EcalSecName+"Energy").c_str(),EcalData,(EcalSecName+"Energy[nEcal]/F").c_str());
+	UPCEcalTree->Branch((EcalSecName+"Energy").c_str(),EcalData,(EcalSecName+"Energy[n"+EcalSecName+"]/F").c_str());
 }
 
 void UPCEcalAnalyzer::analyze(const Event& iEvent, const EventSetup& iSetup)
 {
 	nEcal=0;	
 	Handle<EcalRecHitCollection> ecal_recHits_h;
-	iEvent.getByLabel(EcalColName.c_str(),ecal_recHits_h);
+
+	if(EcalColName[12]=='S'){iEvent.getByLabel("ecalPreshowerRecHit",EcalColName,ecal_recHits_h);}
+	else{iEvent.getByLabel("ecalRecHit",EcalColName,ecal_recHits_h);}
+
 	const EcalRecHitCollection *ecal_recHits = ecal_recHits_h.failedToGet()? 0 : &*ecal_recHits_h;
 	
-	nEcal=ecal_recHits->size();
 	if(ecal_recHits){
+		nEcal=ecal_recHits->size();
 		for(unsigned int i=0; i<ecal_recHits->size();i++){
 			EcalData[i]=(*ecal_recHits)[i].energy();
 		}
@@ -34,5 +37,3 @@ void UPCEcalAnalyzer::analyze(const Event& iEvent, const EventSetup& iSetup)
 
 	UPCEcalTree->Fill();
 }
-
-
