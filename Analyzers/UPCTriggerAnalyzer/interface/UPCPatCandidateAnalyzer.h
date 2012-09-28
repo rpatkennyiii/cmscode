1b9a3691a989288ce2251b98f07cd9bc23529f18
mojoe137 <>
2012-09-28 20:51:18 +0000

    Add muon quality varriables to the candidate tree.

--- Analyzers/UPCTriggerAnalyzer/interface/UPCPatCandidateAnalyzer.h
+++ Analyzers/UPCTriggerAnalyzer/interface/UPCPatCandidateAnalyzer.h
@@ -38,46 +38,61 @@ class UPCPatCandidateAnalyzer : public edm::EDAnalyzer {
       string _hltTrigger;
 
       int acceptedCand;
-      int Reco_QQ_size;       // Number of reconstructed Onia 
-      float Reco_QQ_pt[100000];
-      float Reco_QQ_eta[100000];
-      float Reco_QQ_phi[100000];
-      float Reco_QQ_mass[100000];
-      int Reco_QQ_charge[100000];
-      float Reco_QQ_rapidity[100000];
-      float Reco_QQ_p[100000];
-      float Reco_QQ_energy[100000];
-      float Reco_QQ_et[100000];
-      float Reco_QQ_px[100000];
-      float Reco_QQ_py[100000];
-      float Reco_QQ_pz[100000];
-      float Reco_QQ_theta[100000];
+      int cand_size;       // Number of reconstructed Onia 
+      float cand_pt[100000];
+      float cand_eta[100000];
+      float cand_phi[100000];
+      float cand_mass[100000];
+      int cand_charge[100000];
+      float cand_rapidity[100000];
+      float cand_p[100000];
+      float cand_energy[100000];
+      float cand_et[100000];
+      float cand_px[100000];
+      float cand_py[100000];
+      float cand_pz[100000];
+      float cand_theta[100000];
       bool pass1[100000];
       bool pass2[100000];
       
       //daughter Pat muons from the composite candidate
 
-      float Reco_QQ_mupl_pt[100000];
-      float Reco_QQ_mupl_eta[100000];
-      float Reco_QQ_mupl_phi[100000];
-      float Reco_QQ_mupl_mass[100000];
-      int Reco_QQ_mupl_charge[100000];
-      int Reco_QQ_mupl_StandAlone[100000];
-      int Reco_QQ_mupl_Global[100000];
-      int Reco_QQ_mupl_Tracker[100000];
-      float Reco_QQ_mupl_rapidity[100000];
+      float muon1_pt[100000];
+      float muon1_eta[100000];
+      float muon1_phi[100000];
+      float muon1_mass[100000];
+      int muon1_charge[100000];
+      int muon1_StandAlone[100000];
+      int muon1_Global[100000];
+      int muon1_Tracker[100000];
+      float muon1_rapidity[100000];
+      float muon1_numberOfValidHits[100000];
+      float muon1_pixelLayersWithMeasurement[100000];
+      float muon1_normalizedChi2[100000];
+      float muon1_dz[100000];
+      float muon1_dB[100000];
+      float muon1_numberOfMatchedStations[100000];
+      float muon1_ndof[100000];
 
-      float Reco_QQ_mumi_pt[100000];
-      float Reco_QQ_mumi_eta[100000];
-      float Reco_QQ_mumi_phi[100000];
-      float Reco_QQ_mumi_mass[100000];
-      int Reco_QQ_mumi_charge[100000];
-      int Reco_QQ_mumi_StandAlone[100000];
-      int Reco_QQ_mumi_Global[100000];
-      int Reco_QQ_mumi_Tracker[100000];
-      float Reco_QQ_mumi_rapidity[100000];
+      float muon2_pt[100000];
+      float muon2_eta[100000];
+      float muon2_phi[100000];
+      float muon2_mass[100000];
+      int muon2_charge[100000];
+      int muon2_StandAlone[100000];
+      int muon2_Global[100000];
+      int muon2_Tracker[100000];
+      float muon2_numberOfValidHits[100000];
+      float muon2_pixelLayersWithMeasurement[100000];
+      float muon2_normalizedChi2[100000];
+      float muon2_dz[100000];
+      float muon2_dB[100000];
+      float muon2_numberOfMatchedStations[100000];
+      float muon2_ndof[100000];
+      float muon2_rapidity[100000];
 
-      float dPhi[100000];
+      float cand_dPhi[100000];
+      float cand_vProb[100000];
 
       TTree* candTree; 
 };
